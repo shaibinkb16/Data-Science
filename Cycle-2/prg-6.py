9402dba185d2b01d153e23461f2a0b04982ecc5e
system <shaibinkb16@gmail.com>
2024-07-29 12:21:45 +0530

    Cycle-2

--- Cycle-2/prg-6.py
+++ Cycle-2/prg-6.py
@@ -6,18 +6,17 @@ two_dimensional_array = np.array([[1, 2, 3, 4],
 [5, 6, 7, 8],
 [9, 10, 11, 12],
 [13, 14, 15, 16]])
-print(two_dimensional_array)
+print("origanl Array\n",two_dimensional_array)
 
 excludingFirst=two_dimensional_array[1:]
-print(excludingFirst)
-
+print("Excluding First row \n",excludingFirst)
 excludingLast=two_dimensional_array[:,:-1]
-print(excludingLast)
+print("Excluding Last row \n",excludingLast)
 rowcolumn=two_dimensional_array[1:3,0:2]
-print(rowcolumn)
+print("Elements of 1st and 2 nd columns in the 2nd and 3 rd row \n",rowcolumn)
 column23=two_dimensional_array[:,1:3]
-print(column23)
+print("Elements of 2nd and 3rd column \n",column23)
 element23=two_dimensional_array[:1,1:3]
-print(element23)
+print("2nd and 3rd element of the 1st row:\n",element23)
 selected_elements = two_dimensional_array.ravel()[::-1][4:11]
-print(selected_elements)
+print("Elements from indices 4 to 10 in descending order:\n",selected_elements)
