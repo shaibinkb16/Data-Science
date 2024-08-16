7acec614e5f044cdd8b4203eeab44e7fcc764399
system <shaibinkb16@gmail.com>
2024-08-16 15:57:45 +0530

    test

---
+++ Cycle-3/prg-7.py
@@ -0,0 +1,9 @@
+print("SJC22MCA-2007 : ANJALA MICHAEL")
+print("Batch : MCA 2022-24")
+import pandas as pd
+df = pd.read_csv('iris.csv')
+print("Shape of the dataset is : ",df.shape)
+print("First 5 and last five rows of data set\n",df)
+print("Size of dataset : ",df.size)
+print("No. of samples available for each variety\n",df.count())
+print("Description of the data set\n",df.describe())
